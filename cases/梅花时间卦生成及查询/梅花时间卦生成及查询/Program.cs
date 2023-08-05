bc698ff604d7c7ed99d47c8e6ae6774eb58c46cf
yueyinqiu <lujiaqi2003@126.com>
2023-08-05 11:26:58 +0800

    dmh

--- cases/梅花时间卦生成及查询/梅花时间卦生成及查询/Program.cs
+++ cases/梅花时间卦生成及查询/梅花时间卦生成及查询/Program.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 using LunarCsharpYiJingFrameworkExtensions;
 using YiJingFramework.Annotating.Zhouyi;
 using YiJingFramework.Annotating.Zhouyi.Entities;
-using YiJingFramework.EntityRelationships.MostAccepted.GuaDerivingExtensions;
+using YiJingFramework.EntityRelations.GuaDerivations.Extensions;
 using YiJingFramework.PrimitiveTypes;
 using YiJingFramework.PrimitiveTypes.GuaWithFixedCount;
 
@@ -11,13 +11,13 @@ DateTime dateTime = DateTime.Now;
 Console.WriteLine(dateTime.ToString("yyyy/MM/dd HH:mm"));
 Console.WriteLine();
 
-#region 获取年月日时数 Get the number of year, month, date and hour
+#region 获取年月日时数 Get the number of year, month, day and hour
 if (dateTime.Hour is 23)
     dateTime = dateTime.AddHours(1);
 // 使 23:00 到 24:00 取为后一日。
-// Let 23:00-24:00 considered as the next day.
+// Let 23:00-24:00 to be considered as the next day.
 
-var lunar = Lunar.Lunar.FromDate(dateTime);
+Lunar.Lunar lunar = Lunar.Lunar.FromDate(dateTime);
 // 获取农历时间。
 // Get lunar time.
 
@@ -42,28 +42,28 @@ int timeNumber = lunar.TimeZhi().Index;
 
 #endregion
 
-#region 算卦数 Calculate the numbers about the hexagrams
+#region 算卦数 Calculate the numbers of the hexagrams
 
 int upperNumber = (yearNumber + monthNumber + dayNumber) % 8;
 upperNumber = upperNumber == 0 ? 8 : upperNumber;
 // 《梅花易数》：年月日共计几数以八除之以零数作上卦
-// just do as this two lines to get the number of the upper hexagram
+// just do as the above two lines to get the index of the upper hexagram
 
 int lowerNumber = (yearNumber + monthNumber + dayNumber + timeNumber) % 8;
 lowerNumber = lowerNumber == 0 ? 8 : lowerNumber;
 // 《梅花易数》：年月日数加时之数总计几数以八除之零数作下卦
-// just do as this two lines to get the number of the lower hexagram
+// just do as the above two lines to get the index of the lower hexagram
 
 int changingLineIndex = (yearNumber + monthNumber + dayNumber + timeNumber) % 6;
 changingLineIndex = changingLineIndex == 0 ? 6 : changingLineIndex;
 // 《梅花易数》：就以除六数作动爻
-// just do as this two lines to get the number of the changing line
+// just do as the above two lines to get the index of the changing line
 
 #endregion
 
-#region 取本卦卦画 Get the original hexagram's painting
+#region 取本卦卦画 Get the original hexagram
 
-static GuaTrigram GetTrigramPainting(int numberXiantian)
+static GuaTrigram GetTrigram(int numberXiantian)
 {
     numberXiantian--;
     Debug.Assert(numberXiantian is >= 0b000 and <= 0b111);
@@ -75,44 +75,44 @@ static GuaTrigram GetTrigramPainting(int numberXiantian)
 // 这是通过先天八卦数获取画卦的数学方法。
 // 也可以直接查表：1->☰ 2->☱ 3->☲ 4->☳ 5->☴ 6->☵ 7->☶ 8->☷
 // This is a mathematical method to get the painting through the Xiantian number.
-// You can also directly do this by the map：1->☰ 2->☱ 3->☲ 4->☳ 5->☴ 6->☵ 7->☶ 8->☷
+// You can also directly do this by mapping：1->☰ 2->☱ 3->☲ 4->☳ 5->☴ 6->☵ 7->☶ 8->☷
 
-GuaTrigram upperPainting = GetTrigramPainting(upperNumber);
-GuaTrigram lowerPainting = GetTrigramPainting(lowerNumber);
+GuaTrigram upperTrigram = GetTrigram(upperNumber);
+GuaTrigram lowerTrigram = GetTrigram(lowerNumber);
 // 获取上卦和下卦的卦画。
 // Get the paintings of the upper and the lower trigram.
 
-IEnumerable<Yinyang> originalLines = lowerPainting.Concat(upperPainting);
-GuaHexagram originalPainting = new GuaHexagram(originalLines);
+IEnumerable<Yinyang> originalLines = lowerTrigram.Concat(upperTrigram);
+GuaHexagram originalHexagram = new GuaHexagram(originalLines);
 // 将上卦下卦放在一起得到一个六爻卦，这就是本卦。
 // Put the trigrams together to get a hexagram, which is called the original hexagram.
 
 #endregion
 
-#region 取变卦卦画 Get the changed hexagram's painting
+#region 取变卦卦画 Get the changed hexagram
 
-GuaHexagram changedPainting = originalPainting.ReverseLines(changingLineIndex - 1);
-// 这里使用了 YiJingFramework.EntityRelationships.MostAccepted 提供的拓展方法，
+GuaHexagram changedHexagram = originalHexagram.ChangeLines(changingLineIndex - 1);
+// 这里使用了 YiJingFramework.EntityRelations 提供的拓展方法，
 // 把对应的爻阴阳性质改变，返回新的卦，即变卦。
-// Here we used the extension method provided by the YiJingFramework.EntityRelationships.MostAccepted.
+// Here we used the extension method provided by the YiJingFramework.EntityRelations.
 // The specific line's yin-yang attribute has been changed and a new hexagram has returned, which is called as the changed hexagram.
 
 #endregion
 
-#region 取互卦卦画 Get the overlapping hexagram's painting
+#region 取互卦卦画 Get the overlapping hexagram
 
-GuaHexagram overlappingPainting = originalPainting.Hugua();
-// 仍是 YiJingFramework.EntityRelationships.MostAccepted 包提供的拓展方法，
+GuaHexagram overlappingHexagram = originalHexagram.Hugua();
+// 仍是 YiJingFramework.EntityRelations 包提供的拓展方法，
 // 二三四爻作下卦，三四五爻作上卦产生新的卦，这就是互卦。
-// It's also an extension method provided by the YiJingFramework.EntityRelationships.MostAccepted package.
+// It's also an extension method provided by the YiJingFramework.EntityRelations package.
 // It returns a new hexagram which will be made up of --
 // the second line, the third line, the fourth line,
 // then the third line again, the fourth line again and the fifth line 
 // -- of the original hexagram.
 // This new hexagram is the so-called overlapping hexagram.
 
-if (overlappingPainting == originalPainting)
-    overlappingPainting = changedPainting.Hugua();
+if (overlappingHexagram == originalHexagram)
+    overlappingHexagram = changedHexagram.Hugua();
 // 《梅花易数》：乾坤无互互其变卦
 // If the original is Qian or Kun, which does not have a overlapping hexagram, use the changed's instead. 
 
@@ -126,15 +126,15 @@ static void PrintHexagram(GuaHexagram hexagram)
 }
 
 Console.WriteLine("本卦 THE ORIGINAL");
-PrintHexagram(originalPainting);
+PrintHexagram(originalHexagram);
 Console.WriteLine();
 
 Console.WriteLine("互卦 THE OVERLAPPING");
-PrintHexagram(overlappingPainting);
+PrintHexagram(overlappingHexagram);
 Console.WriteLine();
 
 Console.WriteLine("变卦 THE CHANGED");
-PrintHexagram(changedPainting);
+PrintHexagram(changedHexagram);
 Console.WriteLine();
 
 #endregion
@@ -151,26 +151,24 @@ Debug.Assert(zhouyi is not null);
 // Here the annotation store is downloaded from the internet,
 // You can also load it from elsewhere such as from a local file.
 
-ZhouyiHexagram originalHexagram = zhouyi.GetHexagram(originalPainting);
-ZhouyiHexagramLine changingLine = originalHexagram.EnumerateLines().ElementAt(changingLineIndex - 1);
-ZhouyiHexagram changedHexagram = zhouyi.GetHexagram(changedPainting);
-ZhouyiHexagram overlappingHexagram = zhouyi.GetHexagram(overlappingPainting);
+ZhouyiHexagram originalInZhouyi = zhouyi.GetHexagram(originalHexagram);
+ZhouyiHexagramLine changingLine = originalInZhouyi.EnumerateLines().ElementAt(changingLineIndex - 1);
+ZhouyiHexagram changedInZhouyi = zhouyi.GetHexagram(changedHexagram);
+ZhouyiHexagram overlappingInZhouyi = zhouyi.GetHexagram(overlappingHexagram);
 
-Console.Write($"{originalHexagram.Name}之{changedHexagram.Name}，");
-// Console.Write($"It's {originalHexagram.Name} changing to {changedHexagram.Name}, ");
+Console.Write($"{originalInZhouyi.Name}之{changedInZhouyi.Name}，");
+// Console.Write($"It's {originalInZhouyi.Name} changing to {changedInZhouyi.Name}, ");
 
-var (overlappingUpperPainting, overlappingLowerPainting) = overlappingHexagram.SplitToTrigrams();
-var overlappingUpper = zhouyi.GetTrigram(overlappingUpperPainting);
-var overlappingLower = zhouyi.GetTrigram(overlappingLowerPainting);
-if (overlappingUpperPainting == overlappingLowerPainting)
+(ZhouyiTrigram overlappingUpper, ZhouyiTrigram overlappingLower) = overlappingInZhouyi.SplitToTrigrams(zhouyi);
+if (overlappingUpper.Painting == overlappingLower.Painting)
 {
     Console.WriteLine($"互重{overlappingUpper.Name}。");
-    // Console.Write($"and doubled {overlappingUpper.Name} as the overlapping.");
+    // Console.Write($"and doubled {overlappingUpperInZhouyi.Name} as the overlapping.");
 }
 else
 {
     Console.WriteLine($"互{overlappingUpper.Name}{overlappingLower.Name}。");
-    // Console.Write($"and {overlappingUpper.Name} with {overlappingLower.Name} as the overlapping.");
+    // Console.Write($"and {overlappingUpperInZhouyi.Name} with {overlappingLowerInZhouyi.Name} as the overlapping.");
 }
 
 Console.WriteLine($"易曰：{changingLine.LineText}");
